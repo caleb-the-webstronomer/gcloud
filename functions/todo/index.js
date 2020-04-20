70967fbc923c4ae9c4cdf6f736abcb3bfcad04dc
kaptainti <ceo@techceed.org>
2020-04-19 21:06:26 -0700

    fixed cors

--- functions/todo/index.js
+++ functions/todo/index.js
@@ -6,11 +6,13 @@ const mongodb = require("mongodb");
 const uri = process.env.MONGODB;
 
 exports.todo = (req, res) => {
+  res.set("Access-Control-Allow-Origin", "*");
+  res.set("Access-Control-Allow-Methods", "GET, POST");
   co(function* () {
     const client = yield mongodb.MongoClient.connect(uri);
 
     const docs = yield client.db("Kaleb").collection("todo").find().toArray();
-    res.send("Result: " + JSON.stringify(docs));
+    res.send(docs);
   }).catch((error) => {
     res.send("Error: " + error.toString());
   });
