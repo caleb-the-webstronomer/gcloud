f57043ba7c39d822adfabae184381fbd506f5bae
kaptainti <ceo@techceed.org>
2020-04-19 20:11:15 -0700

    working functions to mongodbatlas

--- functions/todo/index.js
+++ functions/todo/index.js
@@ -1,23 +1,17 @@
 require("dotenv").config();
-import { MongoClient } from "mongodb";
 
-const connectMongoDB = () => MongoClient.connect(process.env.MONGODB);
+const co = require("co");
+const mongodb = require("mongodb");
 
-const getContacts = (req, res) => {
-  return connectMongoDB()
-    .then((db) =>
-      db
-        .collection("todo")
-        .find({})
-        .toArray()
-        .then((documents) => ({ db, documents })),
-    )
-    .then(({ db, documents }) => {
-      db.close();
-      return documents;
-    })
-    .then((contacts) => res.json(contacts))
-    .catch((err) => res.status(400).send(err.toString()));
+const uri = process.env.MONGODB;
+
+exports.todo = (req, res) => {
+  co(function* () {
+    const client = yield mongodb.MongoClient.connect(uri);
+
+    const docs = yield client.db("Kaleb").collection("todo").find().toArray();
+    res.send("Result: " + JSON.stringify(docs));
+  }).catch((error) => {
+    res.send("Error: " + error.toString());
+  });
 };
-
-export const handler = getContacts;
